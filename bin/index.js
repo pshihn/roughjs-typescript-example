98241332c5eef813a9d4d406a307ccb0b9eb461b
Preet Shihn <preetshihn@gmail.com>
2020-04-20 09:26:37 -0700

    .

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,4 @@
 import rough from 'roughjs';
-(() => {
-    const canvas = document.querySelector('canvas');
-    const rc = rough.canvas(canvas);
-    rc.rectangle(10, 10, 100, 100);
-})();
+const canvas = document.querySelector('canvas');
+const rc = rough.canvas(canvas);
+rc.rectangle(10, 10, 100, 100);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import rough from 'roughjs';
 
-(() => {
-  const canvas = document.querySelector('canvas') as HTMLCanvasElement;
-  const rc = rough.canvas(canvas);
-  rc.rectangle(10, 10, 100, 100);
-})();
+const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+const rc = rough.canvas(canvas);
+rc.rectangle(10, 10, 100, 100);
